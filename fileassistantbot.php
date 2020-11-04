13b3883035ae0284395d108514e64965b8bd47bf
xcoders <57432765+xcoders-hub@users.noreply.github.com>
2020-11-05 00:26:45 +0530

    Update fileassistantbot.php

--- fileassistantbot.php
+++ fileassistantbot.php
@@ -10,7 +10,7 @@
  * http://nimah79.ir.
  */
 define('FILES_PATH', __DIR__.'/files');
-define('WEBSERVER_BASE_URL', 'http://yourdomain.com');
+define('WEBSERVER_BASE_URL', 'https://driveuptg.herokuapp.com/');
 define('FILES_EXPIRE_TIME', 24 * 3600); // in seconds
 
 set_time_limit(0);
